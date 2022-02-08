61ba2d4b29cd802a95e1685ee16bec0519573be1
KyuoFoxHuyu <88403766@qq.com>
2022-02-08 14:56:11 +0800

    vince: set TARGET_GAPPS_ARCH to arm64
    Signed-off-by: KyuoFoxHuyu <88403766@qq.com>

--- exthm_vince.mk
+++ exthm_vince.mk
@@ -13,6 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 # Inherit some common exTHmUI stuff
 $(call inherit-product, vendor/exthm/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
 
 # Inherit from vince device.
 $(call inherit-product, device/xiaomi/vince/device.mk)
